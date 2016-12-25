33ae1aa1ff954c1285f6e12c451ee32e67ce9c43
Diamant <diamanthaxhimusa@gmail.com>
2016-12-26 00:53:40 +0100

    finished

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -1,34 +1,48 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template,request, url_for
 from bson import json_util
 from app import mongo
 
 mod_main = Blueprint('main', __name__)
 
-@mod_main.route('/', methods=['GET', 'POST'])
+@mod_main.route('/', methods=['GET'])
 def index():
-    if request.method=="GET":
-        return render_template("indexi.html")
-    elif request.method=="POST":
-        data = request.form.to_dict()
+    if request.method == ['GET']:
+	return render_template('indexii.html')
+    elif request.method == ['POST']:
+ 	data = request.form.to_dict()
         return json_util.dumps(data)
     else:
-        return "bad request"
+        return 'bad request'
 
 
-@mod_main.route('/<string:id>', methods=['GET', 'POST'])
+@mod_main.route('/<string:id>', methods=['GET','POST'])
 def get_doc(id):
     db = mongo.db.arkep
 
-    if request.method=="GET":
-        doc = db.find({"_id":Objectid(id)})
-    	#doc_json = json_util.dumps(doc)
-	#return render_template('doc.html', doc_json )
-	return render_template('doc.html', doc = doc )
-	
+    if request.method==['GET']:
+	doc=db.find_one({"_id":ObjectId(id)})
+	#doc_json=json_util.dumps(doc)
+	#return render_template('doc.html, doc_json)
+	return render_template('doc.html', doc=doc)
     else:
-        return "bad request"
+	    return 'bad request'
+@mod_main.route('/<string:id'>,methods=['GET'] )
+def remove_doc(id):
+    db = mongo.db.arkep
+    if request.method== 'GET':
+	doc = db.remove{{ "_id":ObjectId(id)}}
+	return redirect{url_for('main.lista')}
+    else:
+	return "bad request" 	
 
-@mod_main.route('/lista', methods=['GET'])
+@mod_main.route('/lista',methods=['GET'])
 def lista():
-   dokumentat = mongo.db.arkep.find()
-   return render_template('lista.html', dokumentat = dokumentat)
+    dokumentat = mongo.db.arkep.find()
+    return render_template('lista.html',dokumentat=dokumentat)	
+
+
+
+
+  
+ 	
+   
